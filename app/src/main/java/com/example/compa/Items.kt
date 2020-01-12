d91332a2c5d520b1a3bc38a24e41dd976b886011
machine01 <aaronwcc-wa16@student.tarc.edu.my>
2020-01-13 03:38:33 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/compa/Items.kt
@@ -0,0 +1,14 @@
+package com.example.compa
+
+import android.widget.Button
+
+data class Items(
+    var category: String,
+    var image: Int,
+    var name: String,
+    var price: Double
+
+
+){
+
+}
